b280e5d9de2defa10dff96807d49b2621fecc727
Mateusz Kulec <mati.kulec12@gmail.com>
2025-01-20 21:14:11 +0100

    employer, user qualifications

--- src/app/myaccount/myaccount.component.ts
+++ src/app/myaccount/myaccount.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { UsersService } from '../users.service';
 import { FormsModule } from '@angular/forms';
